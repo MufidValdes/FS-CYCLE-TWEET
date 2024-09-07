f5779a972356fab3f8a49560eb2d76d923290b39
Mufid's Server <muhamadmufidbachri27@gmail.com>
2024-09-07 09:21:12 +0700

    feat: update :memo:

--- src/main.tsx
+++ src/main.tsx
@@ -2,45 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-
-const theme = extendTheme({
-  colors: {
-    tweet: {
-      putih: "#FFFFFF",
-      green: "#04A51E",
-      bg: "#1D1D1D",
-      post: "#005E0E",
-      gray: "#909090",
-    },
-  },
-  sizes: {
-    max: "max-content",
-    min: "min-content",
-    full: "100%",
-    "3xs": "14rem",
-    "2xs": "16rem",
-    xs: "20rem",
-    sm: "24rem",
-    md: "28rem",
-    lg: "32rem",
-    xl: "36rem",
-    "2xl": "42rem",
-    "3xl": "48rem",
-    "4xl": "56rem",
-    "5xl": "64rem",
-    "6xl": "72rem",
-    "7xl": "80rem",
-    "8xl": "90rem",
-    container: {
-      base: "412px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-    },
-  },
-});
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./config/chakra-theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
